10d3bd6f6908f58758181233bab5abb9d12eb414
caijiacheng <caijiacheng@megvii.com>
2020-11-26 18:47:36 +0800

    feat(init): 初始化auth2项目

---
+++ spring-security-oauth2/spring-security-oauth2-server/src/main/java/cn/mrxccc/oauth2/OAuth2ServerApplication.java
@@ -0,0 +1,15 @@
+package cn.mrxccc.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mrxccc
+ * @create 2020/11/26
+ */
+@SpringBootApplication
+public class OAuth2ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2ServerApplication.class, args);
+    }
+}
