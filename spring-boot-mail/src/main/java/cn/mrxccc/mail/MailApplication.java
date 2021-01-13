a81d02d04d8cb4dbbc8f14a756e1436855d5e4aa
caijiacheng <caijiacheng@megvii.com>
2021-01-13 15:57:39 +0800

    feat: 邮件发送配置案例

---
+++ spring-boot-mail/src/main/java/cn/mrxccc/mail/MailApplication.java
@@ -0,0 +1,15 @@
+package cn.mrxccc.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mrxccc
+ * @create 2021/1/13
+ */
+@SpringBootApplication
+public class MailApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MailApplication.class, args);
+    }
+}
