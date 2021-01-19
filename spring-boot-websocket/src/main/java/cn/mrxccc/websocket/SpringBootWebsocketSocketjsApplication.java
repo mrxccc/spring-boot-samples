60d1eb7bf6f806f4def9efb890dbbf16bd3dc313
caijiacheng <caijiacheng@megvii.com>
2021-01-19 11:44:35 +0800

    feat: springboot websocket案例

---
+++ spring-boot-websocket/src/main/java/cn/mrxccc/websocket/SpringBootWebsocketSocketjsApplication.java
@@ -0,0 +1,16 @@
+package cn.mrxccc.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mrxccc
+ * @create 2021/1/19
+ */
+@SpringBootApplication
+public class SpringBootWebsocketSocketjsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebsocketSocketjsApplication.class, args);
+    }
+
+}
---
+++ spring-boot-websocket/src/main/java/cn/mrxccc/websocket/config/WebSocketServerConfigure.java
@@ -0,0 +1,26 @@
+package cn.mrxccc.websocket.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import cn.mrxccc.websocket.handle.MyStringWebSocketHandler;
+
+/**
+ * @author mrxccc
+ * @create 2021/1/19
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketServerConfigure implements WebSocketConfigurer {
+
+    @Autowired
+    private MyStringWebSocketHandler myStringWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myStringWebSocketHandler, "/connect").withSockJS();
+    }
+}
