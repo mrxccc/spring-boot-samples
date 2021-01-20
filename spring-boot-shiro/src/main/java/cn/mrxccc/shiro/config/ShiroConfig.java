50f42970e59cb86c2c6b5b6b824aaacf64e00339
caijiacheng <caijiacheng@megvii.com>
2021-01-20 14:53:12 +0800

    feat: springboot shiro redis cache案例

--- spring-boot-shiro/src/main/java/cn/mrxccc/shiro/config/ShiroConfig.java
+++ spring-boot-shiro/src/main/java/cn/mrxccc/shiro/config/ShiroConfig.java
@@ -4,18 +4,25 @@ import org.apache.shiro.cache.ehcache.EhCacheManager;
 import org.apache.shiro.codec.Base64;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.session.SessionListener;
+import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.session.mgt.eis.SessionDAO;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.servlet.SimpleCookie;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.crazycake.shiro.RedisCacheManager;
+import org.crazycake.shiro.RedisSessionDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Properties;
 
@@ -105,7 +112,6 @@ public class ShiroConfig {
         return simpleMappingExceptionResolver;
     }
 
-
     @Bean
     public AuthorizingRealm shiroRealm(){
         // 配置Realm，需自己实现
@@ -183,5 +189,122 @@ public class ShiroConfig {
         redisCacheManager.setExpire(200000);
         return redisCacheManager;
     }
+
+    /**
+     * SessionDAO的作用是为Session提供CRUD并进行持久化的一个shiro组件
+     * MemorySessionDAO 直接在内存中进行会话维护
+     * EnterpriseCacheSessionDAO  提供了缓存功能的会话维护，默认情况下使用MapCache实现，内部使用ConcurrentHashMap保存缓存的会话。
+     * @return
+     */
+    @Bean
+    public SessionDAO sessionDAO() {
+        RedisSessionDAO redisSessionDAO = new RedisSessionDAO();
+        redisSessionDAO.setRedisManager(redisConfig.redisManager());
+        //session在redis中的保存时间,最好大于session会话超时时间
+        redisSessionDAO.setExpire(12000);
+        return redisSessionDAO;
+    }
+
+    /**
+     * 配置session监听
+     * @return
+     */
+    @Bean("sessionListener")
+    public ShiroSessionListener sessionListener(){
+        ShiroSessionListener sessionListener = new ShiroSessionListener();
+        return sessionListener;
+    }
+
+    /**
+     * 配置保存sessionId的cookie
+     * 注意：这里的cookie 不是上面的记住我 cookie 记住我需要一个cookie session管理 也需要自己的cookie
+     * 默认为: JSESSIONID 问题: 与SERVLET容器名冲突,重新定义为sid
+     * @return
+     */
+    @Bean("sessionIdCookie")
+    public SimpleCookie sessionIdCookie(){
+        //这个参数是cookie的名称
+        SimpleCookie simpleCookie = new SimpleCookie("sid");
+        //setcookie的httponly属性如果设为true的话，会增加对xss防护的安全系数。它有以下特点：
+
+        //setcookie()的第七个参数
+        //设为true后，只能通过http访问，javascript无法访问
+        //防止xss读取cookie
+        simpleCookie.setHttpOnly(true);
+        simpleCookie.setPath("/");
+        //maxAge=-1表示浏览器关闭时失效此Cookie
+        simpleCookie.setMaxAge(-1);
+        return simpleCookie;
+    }
+
+    /**
+     * 配置会话管理器，设定会话超时及保存
+     * @return
+     */
+    @Bean("sessionManager")
+    public SessionManager sessionManager() {
+        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+        Collection<SessionListener> listeners = new ArrayList<SessionListener>();
+        //配置监听
+        listeners.add(sessionListener());
+        sessionManager.setSessionListeners(listeners);
+        sessionManager.setSessionIdCookie(sessionIdCookie());
+        sessionManager.setSessionDAO(sessionDAO());
+        sessionManager.setCacheManager(getRedisCacheManager());
+
+        //全局会话超时时间（单位毫秒），默认30分钟  暂时设置为10秒钟 用来测试
+        sessionManager.setGlobalSessionTimeout(1800000);
+        //是否开启删除无效的session对象  默认为true
+        sessionManager.setDeleteInvalidSessions(true);
+        //是否开启定时调度器进行检测过期session 默认为true
+        sessionManager.setSessionValidationSchedulerEnabled(true);
+        //设置session失效的扫描时间, 清理用户直接关闭浏览器造成的孤立会话 默认为 1个小时
+        //设置该属性 就不需要设置 ExecutorServiceSessionValidationScheduler 底层也是默认自动调用ExecutorServiceSessionValidationScheduler
+        //暂时设置为 5秒 用来测试
+        sessionManager.setSessionValidationInterval(3600000);
+        //取消url 后面的 JSESSIONID
+        sessionManager.setSessionIdUrlRewritingEnabled(false);
+        return sessionManager;
+
+    }
+
+    /**
+     * 并发登录控制
+     * @return
+     */
+//    @Bean
+//    public KickoutSessionControlFilter kickoutSessionControlFilter(){
+//        KickoutSessionControlFilter kickoutSessionControlFilter = new KickoutSessionControlFilter();
+//        //用于根据会话ID，获取会话进行踢出操作的；
+//        kickoutSessionControlFilter.setSessionManager(sessionManager());
+//        //使用cacheManager获取相应的cache来缓存用户登录的会话；用于保存用户—会话之间的关系的；
+//        kickoutSessionControlFilter.setCacheManager(cacheManager());
+//        //是否踢出后来登录的，默认是false；即后者登录的用户踢出前者登录的用户；
+//        kickoutSessionControlFilter.setKickoutAfter(false);
+//        //同一个用户最大的会话数，默认1；比如2的意思是同一个用户允许最多同时两个人登录；
+//        kickoutSessionControlFilter.setMaxSession(1);
+//        //被踢出后重定向到的地址；
+//        kickoutSessionControlFilter.setKickoutUrl("/login?kickout=1");
+//        return kickoutSessionControlFilter;
+//    }
+
+    /**
+     * 配置密码比较器
+     * @return
+     */
+//    @Bean("credentialsMatcher")
+//    public RetryLimitHashedCredentialsMatcher retryLimitHashedCredentialsMatcher(){
+//        RetryLimitHashedCredentialsMatcher retryLimitHashedCredentialsMatcher = new RetryLimitHashedCredentialsMatcher(cacheManager());
+//
+//        //如果密码加密,可以打开下面配置
+//        //加密算法的名称
+//        //retryLimitHashedCredentialsMatcher.setHashAlgorithmName("MD5");
+//        //配置加密的次数
+//        //retryLimitHashedCredentialsMatcher.setHashIterations(1024);
+//        //是否存储为16进制
+//        //retryLimitHashedCredentialsMatcher.setStoredCredentialsHexEncoded(true);
+//
+//        return retryLimitHashedCredentialsMatcher;
+//    }
 
 }
